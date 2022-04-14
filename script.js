a5d188453450ed7dbc18e9b5139c46d7d8901536
Achi-Simonishvili <98604349+Achi-Simonishvili@users.noreply.github.com>
2022-04-14 17:11:31 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+// 1.
+// let array = [5, 25, 89, 120, 36];
+
+// array.push("javascript", "python");
+// array.unshift("html", "css");
+// array.shift();
+// array.pop();
+// console.log(array);
+// console.log(array.length);
+
+// 2.
+// let array = ["Orange", "Banana", "Pear"];
+// // console.log(array.length);
+
+// array.push("Apple", "Pineapple");
+// array.unshift("Watermelon");
+// array.splice(2, 0, "Mango");
+// array.shift();
+// array.pop();
+
+// console.log(array);
+// console.log(array.length);
+
+// 3.
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+// let newArray = array.map((item) => item / 3);
+// console.log(newArray);
+
+// 4.
+// let array = [
+//   "hello",
+//   125,
+//   "javascript",
+//   "html",
+//   43,
+//   "css",
+//   "scss",
+//   "bootstrap",
+//   88,
+//   59,
+//   "python",
+// ];
+
+// let numbers = array.filter((item) => typeof item == "number");
+// console.log(numbers);
+
+// 5.
+// let languages = ["html", "css", "javascript", "python", "php"];
+
+// let x = languages.filter(function (item) {
+//   return item.length > 3;
+// });
+
+// console.log(x);
+
+// 6.
+// let array= ['academy', 'of', 'digital', 'industries'];
+// let sentence = array.reduce((previousValue, currentValue) => previousValue + ' ' + currentValue);
+
+// console.log(sentence);
+
+// 7.
+// let item = [12, "google", 32, null, "yahoo", 25];
+
+// let array = item.map(function (x) {
+//   if (typeof x == "number") {
+//     return x ** 2;
+//   } else if (typeof x == "string") {
+//     return x.toUpperCase();
+//   } else {
+//     return x;
+//   }
+// });
+
+// console.log(array);
+
+// 8.
+// let words = ["Madrid", "Rome", "Milan", "Berlin"];
+// let cities = words.filter((name) => name.includes("m") || name.includes("M"));
+// console.log(cities);
